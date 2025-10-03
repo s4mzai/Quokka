59ce913c2e854439e99eb5fafdb06bb9ef23d546
samar <hi.samar.7x@gmail.com>
2025-10-03 23:44:48 +0530

    Add Section to Landing Page

---
+++ frontend/src/components/LandingPage/Section.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+const Section = () => {
+  return (
+    <div className="mb-10 md:mb-0 md:h-screen flex flex-col gap-10 px-10 md:gap-20 items-center justify-center text-center mt-40 lg:mt-0">
+        <h1 className="text-5xl md:text-7xl font-bold">Skip the <span className="bg-clip-text text-transparent bg-[radial-gradient(circle_at_top_left,_#10b981,_#84cc16,_#14b8a6)]  bg-[length:200%_200%] animate-gradient-move">Boring</span>, Keep the Story</h1>
+        <p className="hidden md:block text-xl font-medium text-gray-600 md:w-[70%]">Life’s too short to drown in never-ending PDFs, articles, and study notes. Summarize instantly, skip the clutter, keep the essence, and move forward smarter, faster, lighter, always</p>
+        <p className="block md:hidden text-xl font-medium text-gray-600 md:w-[70%]">Life’s too short to pretend you’ll “read it later.” Summarize it now. And move on.</p>
+        <Button className="bg-black focus-visible:ring-0 text-xl font-medium h-12 w-40  rounded-full transform hover:-translate-y-1 transition duration-400 cursor-pointer text-white  bg-[radial-gradient(circle_at_top_left,_#10b981,_#84cc16,_#14b8a6)]  bg-[length:200%_200%] animate-gradient-move">
+            Try Quokko              
+        </Button>
+    </div>
+  )
+}
+
+export default Section
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import HomeNavbar from "@/components/LandingPage/Navbar"
+import Section from "@/components/LandingPage/Section"
 
 
 const Home = () => {
   return (
     <>
         <HomeNavbar/>
+        <Section/>
     </>
   )
 }
